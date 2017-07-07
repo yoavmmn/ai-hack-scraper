e0517c719236c0e084551e2be3ebbae874b51987
Yoav Maman <yoavmmn@gmail.com>
2017-07-07 08:34:19 +0300

    clear \n's from scraped content

--- scraper.py
+++ scraper.py
@@ -1,31 +1,8 @@
 from flask import Response
-from scrapy.crawler import CrawlerProcess
-from scrapy.spiders import BaseSpider
-import scrapy
-import html2text
 import json
 import urllib
 import requests
 import lxml.html
-
-class Spider(scrapy.Spider):
-  name = "AI-Hack Spider"
-  allowed_domain = []
-  start_urls = []
-
-  def parse(self, response):
-    results = []
-
-    results.append(self.getHtml(response))
-
-    return results
-
-  def getHtml(self, response):
-    hxs = scrapy.Selector(response)
-
-    return {
-      "text": ''.join(hxs.xpath("//body//text()").extract()).strip()
-    }
 
 
 def scraper(request, logger):
@@ -37,22 +14,10 @@ def scraper(request, logger):
 
   logger.debug("url {0}".format(url))
 
-  # process = CrawlerProcess({
-  #   "USER_AGENT": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
-  #   })
-
-  # Spider.start_urls = [url]
-  # Spider.allowed_domain = [url]
-
-  # logger.debug("allowed_domain {0} start_urls {1}".format(Spider.allowed_domain, Spider.start_urls))
-
-  # process.crawl(Spider)
-  # data = process.start()
-
-  # logger.debug("scraped motherfucker {0}".format(data))
-  # 
   r = requests.get(url)
   tree = lxml.html.fromstring(r.text)
   data = tree.xpath("//body")[0].text_content()
+
+  data = ''.join(data.split('\n'))
 
   return str({"text": data})
