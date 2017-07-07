7e8120d700f027b7a2a868b99e133a718b517011
Yoav Maman <yoavmmn@gmail.com>
2017-07-07 09:19:34 +0300

    return an array of relevant keywords

--- scraper.py
+++ scraper.py
@@ -23,9 +23,10 @@ def scraper(request, logger):
 
   data = ''.join(data.split('\n'))
 
-  keywords = indicoio.keywords(data, version=2)
-  for keyword in keywords:
-    logger.debug("{0}".format(keyword))
+  keyowrds = []
+  analysis = indicoio.keywords(data, version=2)
+  for keyword in analysis:
+    keywords.append(keyword)
 
   return str({
     "text": data,
