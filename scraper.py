a92f113acb622ba9a5d3c2a722e2e6b62c668a2f
Yoav Maman <yoavmmn@gmail.com>
2017-07-07 04:05:51 +0300

    Initial commit 🚀

---
+++ scraper.py
@@ -0,0 +1,14 @@
+from flask import Response
+import json
+import urllib
+
+def scraper(request, logger):
+  request_data = request.get_data()
+  logger.debug("received {0}".format(request_data))
+
+  url = "".join(request_data.split("url="))
+  url = urllib.unquote(url)
+
+  logger.debug("url {0}".format(url))
+
+  return Response(status=200)
---
+++ server.py
@@ -0,0 +1,23 @@
+import logging
+
+from scraper import scraper
+from flask import Flask, request, Response
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+config = {
+  "port": 34200
+}
+
+app = Flask(__name__)
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+  return scraper(request, logger)
+
+app.run(host='0.0.0.0', port=config["port"], debug=True)
